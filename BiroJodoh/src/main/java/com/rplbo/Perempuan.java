088d707f0365922ad1cfdf6554cf67029fcb1997
freanymellynusmany <78341427+freanymellynusmany@users.noreply.github.com>
2022-04-04 16:17:10 +0700

    Add files via upload

---
+++ BiroJodoh/src/main/java/com/rplbo/Perempuan.java
@@ -0,0 +1,15 @@
+package com.rplbo;
+
+public class Perempuan {
+    private string nama;
+    private int usia;
+    private double jumlahcinta;
+    private jodoh pria;
+    private  temankencan pria;
+    private double standarjumlahcinta;
+    public void mengajak
+
+    public void Mengajakkencan(void pria) {
+        this.pria = pria;
+    }
+}
---
+++ BiroJodoh/src/main/java/com/rplbo/pria.java
@@ -0,0 +1,10 @@
+package com.rplbo;
+
+public class pria {
+    private string nama;
+    private int usia;
+    private string status;
+    private double jumlahcinta;
+private perempuan jodoh;
+private temanKencan perempuan};
+
